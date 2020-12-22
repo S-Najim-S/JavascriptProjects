b369a0cd00ca24838b05fbf4560cfcf50c3a5305
SayedNajimullah <saidnajimullahs@yahoo.com>
2020-12-23 00:55:54 +0100

    FCB quiz app logic added

--- quiz-app/js/app.js
+++ quiz-app/js/app.js
@@ -4,8 +4,12 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-
+const answerEls = document.querySelectorAll(".answer");
 const myBtn = document.getElementById("submit-btn");
+const quiz = document.getElementById("quiz");
+
+let currentQuestion = 0;
+let score = 0;
 
 const myQuiz = [
 	{
@@ -34,11 +38,11 @@ const myQuiz = [
 	},
 	{
 		question: "Who was the head coach of the club in the 2018/19 season?",
-		a: "1899",
-		b: "1900",
-		c: "1908",
-		d: "1921",
-		correct: "a",
+		a: "Guardiola",
+		b: "Valverde",
+		c: "Luis E.",
+		d: "Koeman",
+		correct: "b",
 	},
 	{
 		question: "Who scored the most number of goals in the history of club?",
@@ -50,36 +54,53 @@ const myQuiz = [
 	},
 ];
 
-let currentQuestion = 0;
-
 const getSelected = () => {
-	const answer = document.querySelectorAll(".answer");
+	let answer = undefined;
 
-	answer.forEach((answer) => {
-		console.log(answer.checked);
+	answerEls.forEach((answerEl) => {
+		if (answerEl.checked) {
+			answer = answerEl.id;
+		}
 	});
+	return answer;
 };
 
 const loadQuiz = () => {
+	deselectQuestion();
 	const currentQuizData = myQuiz[currentQuestion];
-
 	questionEl.innerHTML = currentQuizData.question;
-
 	a_text.innerText = currentQuizData.a;
 	b_text.innerText = currentQuizData.b;
 	c_text.innerText = currentQuizData.c;
 	d_text.innerText = currentQuizData.d;
 };
 
-myBtn.addEventListener("click", () => {
-	currentQuestion++;
+const deselectQuestion = () => {
+	answerEls.forEach((answerEl) => {
+		if (answerEl.checked) {
+			answerEl.checked = false;
+		}
+	});
+};
 
-	getSelected();
-	// if (currentQuestion < myQuiz.length) {
-	// 	loadQuiz();
-	// } else {
-	// 	alert("You are finished! this is your Score!");
-	// }
+myBtn.addEventListener("click", () => {
+	// check for answer
+	const answer = getSelected();
+
+	if (answer) {
+		if (answer === myQuiz[currentQuestion].correct) {
+			console.log("Inside score++" + score);
+			score++;
+		}
+		currentQuestion++;
+		if (currentQuestion < myQuiz.length) {
+			loadQuiz();
+		} else {
+			quiz.innerHTML = `<h2>You have answered ${score}/${myQuiz.length}.</h2>
+            <button onclick="location.reload()">Reload</button>
+            `;
+		}
+	}
 });
 
 loadQuiz();
