a3aaf0c25b31c37aad96339be5b077e4e6a16107
unknown <saidnajimullahs@yahoo.com>
2021-01-17 18:45:22 +0100

    worked more with finish and active button and added functionality to moreinfo button

--- Project planner/assets/scripts/app.js
+++ Project planner/assets/scripts/app.js
@@ -1,4 +1,10 @@
 class DOMHelper {
+	static clearEventListneds(element) {
+		const clonedEl = element.cloneNode(true);
+		element.replaceWith(clonedEl);
+		return clonedEl;
+	}
+
 	static moveElement(elementID, newDestinationSelector) {
 		const element = document.getElementById(elementID);
 		const destinationEl = document.querySelector(newDestinationSelector);
@@ -6,26 +12,91 @@ class DOMHelper {
 	}
 }
 
-class ToolTip {}
+class Component {
+	constructor(hostElId, insertBefore = false) {
+		if (hostElId) {
+			this.hostEl = document.getElementById(hostElId);
+		} else {
+			this.hostEl = document.body;
+		}
+		this.insertBefore = insertBefore;
+	}
+	detach() {
+		if (this.element) {
+			this.element.remove();
+		}
+	}
+	attach() {
+		console.log(this.insertBefore);
+
+		this.hostEl.insertAdjacentElement(
+			this.insertBefore ? 'afterbegin' : 'beforeend',
+			this.element
+		);
+	}
+}
+
+class ToolTip extends Component {
+	constructor(closeNotifierFn) {
+		super('active-projects', true);
+		this.closeNotifier = closeNotifierFn;
+		this.create();
+	}
+	closeToolTip = () => {
+		this.detach();
+		this.closeNotifier();
+	};
+	create() {
+		const tooltipElement = document.createElement('div');
+		tooltipElement.className = 'card';
+		tooltipElement.textContent = 'Hello Dear!!!, click on me to delete :)';
+		tooltipElement.addEventListener('click', this.closeToolTip);
+		this.element = tooltipElement;
+	}
+}
 
 class ProjectItem {
-	constructor(id, updateProjectListsFunction) {
+	hasActiveTooltip = false;
+
+	constructor(id, updateProjectListsFunction, type) {
 		this.id = id;
 		this.updateProjectListsHandler = updateProjectListsFunction;
 		this.connectMoreInfoBtn();
-		this.connectSwitchBtn();
+		this.connectSwitchBtn(type);
 	}
 
-	connectMoreInfoBtn() {}
+	showMoreInfo() {
+		if (this.hasActiveTooltip) {
+			return;
+		}
+		const tooltip = new ToolTip(() => {
+			this.hasActiveTooltip = false;
+		});
+		tooltip.attach();
+		this.hasActiveTooltip = true;
+	}
 
-	connectSwitchBtn() {
+	connectMoreInfoBtn() {
 		const projectItemEl = document.getElementById(this.id);
-		const switchBtn = projectItemEl.querySelector('button:last-of-type');
+		const moreInfoBtn = projectItemEl.querySelector('button:first-of-type');
 
+		moreInfoBtn.addEventListener('click', this.showMoreInfo);
+	}
+
+	connectSwitchBtn(type) {
+		const projectItemEl = document.getElementById(this.id);
+		let switchBtn = projectItemEl.querySelector('button:last-of-type');
+		switchBtn = DOMHelper.clearEventListneds(switchBtn);
+		switchBtn.textContent = type === 'active' ? 'Finish' : 'Active';
 		switchBtn.addEventListener(
 			'click',
 			this.updateProjectListsHandler.bind(null, this.id)
 		);
+	}
+
+	update(updateProjectListsFunction, type) {
+		this.updateProjectListsHandler = updateProjectListsFunction;
+		this.connectSwitchBtn(type);
 	}
 }
 
@@ -38,7 +109,7 @@ class ProjectList {
 
 		for (const prjItem of prjItems) {
 			this.projects.push(
-				new ProjectItem(prjItem.id, this.switchProject.bind(this))
+				new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
 			);
 		}
 		console.log(this.projects);
@@ -52,6 +123,9 @@ class ProjectList {
 	addProject(project) {
 		this.projects.push(project);
 		DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+		console.log(`#${this.type}-projects ul`);
+
+		project.update(this.switchProject.bind(this), this.type);
 	}
 
 	// remove project from projects list
