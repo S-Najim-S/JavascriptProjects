2c9e027cd31b5104e726125dbbd292fe7124e550
SayedNajimullah <saidnajimullahs@yahoo.com>
2021-02-17 21:43:20 +0100

    Fetching random meal from Api

---
+++ RecipeApp/script.js
@@ -0,0 +1,49 @@
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal() {
+  const response = await fetch(
+    " https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+
+  const respData = await response.json();
+  const randomMeal = respData.meals[0];
+  console.log(randomMeal);
+
+  loadRandomMeal(randomMeal, true);
+}
+
+async function getMealById() {
+  const meal = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+}
+
+async function getMealsByName() {
+  const meals = await fetch(
+    ` https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+  );
+}
+
+function loadRandomMeal(mealData, random = true) {
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
+
+  meal.innerHTML = `
+    <div class="meal-header">
+        ${random ? `<span class="random">Random Recipe</span>` : ""}
+        <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+    </div>
+    <div class="meal-body">
+        <h4>${mealData.strMeal}</h4>
+        <button class="fav-btn"><i class="fa fa-heart" aria-hidden="true"></i>
+        </button>
+    </div>`;
+
+  const myBtn = meal.querySelector(".meal-body .fav-btn");
+  myBtn.addEventListener("click", () => {
+    myBtn.classList.toggle("active");
+  });
+  meals.appendChild(meal);
+}
