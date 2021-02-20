bf75f6b553f498a618dc20fbcb8c88b245630c5d
SayedNajimullah <saidnajimullahs@yahoo.com>
2021-02-20 20:24:57 +0100

    Search recipe added

--- RecipeApp/script.js
+++ RecipeApp/script.js
@@ -1,5 +1,10 @@
-const meals = document.getElementById("meals");
+const mealsEl = document.getElementById("meals");
 const favContainer = document.getElementById("fav-meals");
+const recipeInfo = document.getElementById("recipe-info");
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
+const maelPopup = document.getElementById("meal-popup");
+const closePopupBtn = document.getElementById("close-popup");
 
 getRandomMeal();
 fetchFavMeals();
@@ -12,7 +17,7 @@ async function getRandomMeal() {
   const respData = await response.json();
   const randomMeal = respData.meals[0];
 
-  loadRandomMeal(randomMeal, true);
+  addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
@@ -23,17 +28,21 @@ async function getMealById(id) {
   const respData = await response.json();
   const meal = respData.meals[0];
 
-  console.log(meal);
   return meal;
 }
 
-async function getMealsByName() {
-  const meals = await fetch(
+async function getMealsByName(name) {
+  const response = await fetch(
     ` https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
   );
+
+  const respData = await response.json();
+  const meals = respData.meals;
+
+  return meals;
 }
 
-function loadRandomMeal(mealData, random = true) {
+function addMeal(mealData, random = true) {
   const meal = document.createElement("div");
   meal.classList.add("meal");
 
@@ -94,7 +103,6 @@ async function fetchFavMeals() {
     meal = await getMealById(mealId);
     addMealToFav(meal);
   }
-  console.log(meals);
 }
 
 function addMealToFav(mealData) {
@@ -116,3 +124,49 @@ function addMealToFav(mealData) {
 
   favContainer.appendChild(favMeal);
 }
+
+function showMealInfo(mealData) {
+  //update the popup
+
+  const mealEl = document.createElement("div");
+
+  mealEl.innerHTML = `
+      <h1>${mealData.strMeal}</h1>
+      <img
+        src="${mealData.strMealThumb}"
+        alt="${mealData.strMeal}"
+      />
+      <div>
+        <p>
+        ${mealData.strInstructions}
+        </p>
+        <ul>
+          <li>Ing 1 / Measure</li>
+          <li>Ing 1 / Measure</li>
+          <li>Ing 1 / Measure</li>
+        </ul>`;
+
+  recipeInfo.appendChild(mealEl);
+
+  // show the recipe
+
+  recipeInfo.classList.remove("hidden");
+}
+
+searchBtn.addEventListener("click", async () => {
+  mealsEl.innerHTML = "";
+  const search = searchTerm.value;
+
+  const meals = await getMealsByName(search);
+
+  if (meals) {
+    meals.forEach((meal) => {
+      addMeal(meal);
+    });
+  }
+});
+
+closePopupBtn.addEventListener("click", () => {
+  console.log("Hello");
+  maelPopup.classList.add("hidden");
+});
