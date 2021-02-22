7b8237aa1e5468d7d408fe0278d05d5843a1d537
SayedNajimullah <saidnajimullahs@yahoo.com>
2021-02-22 13:39:28 +0100

    a bug fixd

--- noteApp/js/app.js
+++ noteApp/js/app.js
@@ -3,11 +3,20 @@ const notesEl = document.querySelector(".notes");
 
 const notes = JSON.parse(localStorage.getItem("notes"));
 
-const addNewNote = (text = "") => {
-	const note = document.createElement("div");
-	note.classList.add("note");
+if (notes) {
+  notes.forEach((note) => {
+    addNewNote(note);
+  });
+}
+addBtn.addEventListener("click", () => {
+  addNewNote();
+});
 
-	note.innerHTML = `
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+
+  note.innerHTML = `
     <div class="notes">
         <div class="toolbar">
             <button class="edit"><i class="fa fa-pencil"></i></button>
@@ -17,50 +26,43 @@ const addNewNote = (text = "") => {
         <textarea class="${text ? "hidden" : ""}"></textarea>
     </div> `;
 
-	const deleteBtn = note.querySelector(".delete");
-	const editBtn = note.querySelector(".edit");
-	const textArea = note.querySelector("textarea");
-	const main_text = note.querySelector(".main_text");
+  const deleteBtn = note.querySelector(".delete");
+  const editBtn = note.querySelector(".edit");
+  const textArea = note.querySelector("textarea");
+  const main_text = note.querySelector(".main_text");
 
-	textArea.value = text;
-	main_text.innerHTML = text;
+  textArea.value = text;
+  main_text.innerHTML = text;
 
-	// toggling the hidden class for main-text and textarea
-	editBtn.addEventListener("click", () => {
-		main_text.classList.toggle("hidden");
-		textArea.classList.toggle("hidden");
-	});
+  // toggling the hidden class for main-text and textarea
+  editBtn.addEventListener("click", () => {
+    main_text.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
 
-	// remove note
-	deleteBtn.addEventListener("click", () => {
-		note.remove();
-		updateLocalStorage();
-	});
-	// Typing inside textarea
-	textArea.addEventListener("input", (e) => {
-		const { value } = e.target;
-		main_text.innerHTML = marked(value);
-		updateLocalStorage();
-	});
+  // remove note
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+    updateLocalStorage();
+  });
+  // Typing inside textarea
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+    main_text.innerHTML = marked(value);
+    updateLocalStorage();
+  });
 
-	// add new Note
-	document.body.appendChild(note);
+  // add new Note
+  document.body.appendChild(note);
 };
 
 const updateLocalStorage = () => {
-	const notesText = document.querySelectorAll("textarea");
+  const notesText = document.querySelectorAll("textarea");
 
-	const notes = [];
+  const notes = [];
 
-	notesText.forEach((note) => {
-		notes.push(note.value);
-	});
-	localStorage.setItem("notes", JSON.stringify(notes));
+  notesText.forEach((note) => {
+    notes.push(note.value);
+  });
+  localStorage.setItem("notes", JSON.stringify(notes));
 };
-
-if (notes) {
-	notes.forEach((note) => {
-		addNewNote(note);
-	});
-}
-addBtn.addEventListener("click", addNewNote);
